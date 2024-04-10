6f9ee893fd9b96bfc640c4dac54baa8cee47c586
garold664 <garold664@gmail.com>
2024-04-10 09:03:25 +0300

    retrieving the first page with requested books

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react';
 
 import './App.css';
+import useBookSearch from './useBookSearch';
 
 function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
 
+  const books = useBookSearch(query, pageNumber);
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+    setPageNumber(1);
+  }
+
+  const booksList = books.map((book) => <div key={book}>{book}</div>);
+
   return (
     <>
-      <input type="text" />
-      <div>Title</div>
+      <input type="text" onChange={handleSearch} />
+      {booksList}
+      {/* <div>Title</div>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
       <div>Loading</div>
-      <div>Error</div>
+      <div>Error</div> */}
     </>
   );
 }
---
+++ src/useBookSearch.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function useBookSearch(query: string, pageNumber: number) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+
+    let cancel;
+    axios({
+      method: 'GET',
+      url: `https://openlibrary.org/search.json`,
+      params: {
+        q: query,
+        page: pageNumber,
+      },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setBooks((prevBooks: string[]) => {
+          return [
+            ...prevBooks,
+            ...res.data.docs.map((b: { title: string }) => b.title),
+          ];
+        });
+        console.log(res.data);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+  }, [query, pageNumber]);
+
+  return books;
+}
