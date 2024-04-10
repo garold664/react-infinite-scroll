4bb1b9b17fb82c963b4a21227e1a63d3c1dd384c
garold664 <garold664@gmail.com>
2024-04-10 13:14:49 +0300

    implemented the infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
 import './App.css';
 import useBookSearch from './useBookSearch';
@@ -9,19 +9,46 @@ function App() {
 
   const { books, loading, error, hasMore } = useBookSearch(query, pageNumber);
 
+  const observer = useRef<IntersectionObserver>();
+  const lastBookElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          console.log('Visibel');
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      console.log(node);
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
     setQuery(event.target.value);
     setPageNumber(1);
   }
 
-  const booksList = books.map((book) => <div key={book}>{book}</div>);
+  const booksList = books.map((book, index) => {
+    if (books.length === index + 1) {
+      return (
+        <div ref={lastBookElementRef} key={book}>
+          {book}
+        </div>
+      );
+    } else {
+      return <div key={book}>{book}</div>;
+    }
+  });
 
   return (
     <>
       <input type="text" onChange={handleSearch} />
+      {booksList.length > 0 && booksList}
       {loading && <div>Loading...</div>}
       {error && <div>Error</div>}
-      {booksList.length > 0 && booksList}
     </>
   );
 }
